924d98142de7348007c4c8dab29d1aa2a0283c86
bermaxim <maks.berezovskiy.2005@gmail.com>
2023-11-30 10:26:43 +0300

    last..

--- menu.c
+++ menu.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#define N 100
-
 #include "abs_max.h"
 #include "abs_min.h"
 #include "diff.h"
@@ -11,39 +9,7 @@
 
 int main(){
 
-    // int array[N];
-    // int i = 0;
-
-    // while (getchar() != '\n'){
-    //     scanf("%d", &array[i]);
-    //     i++;
-    // }
-
-    // int len = i;
-
-    // int index_max = abs_max(array, len);
-    // int index_min = abs_min(array, len);
-
     print_res();
-
-    // switch (array[0])
-    // {
-    // case 0:
-    //     printf("%d\n", array[index_max]);
-    //     break;
-    // case 1:                            
-    //     printf("%d\n", array[index_min]);
-    //     break;
-    // case 2:
-    //     printf("%d\n", diff(array, index_max, index_min));
-    //     break;
-    // case 3:
-    //     printf("%d\n", sum(array, len, index_max));
-    //     break;
-    // default:
-    //     printf("Данные некорректны\n");
-    //     break;
-    // }
 
     return 0;
 }
